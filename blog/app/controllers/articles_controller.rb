38c92ddd86dcf19b36d5b39e515151af0d09ebe7
qqdipps <sdipp007@gmail.com>
2018-09-25 13:50:33 -0700

    Added save to create methood. Added private article_params method. \nAdded show method. Created show article. App allows for article to be saved and displayed.

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,12 +1,25 @@
 class ArticlesController < ApplicationController
 
+	def show 
+		@article = Article.find(params[:id])
+	end
+	def new
+	end
 
-def new
-end
+
+	def create
+	# Ruby secuirity (strong parameters)  (params[:article]) does not work  
+	# @article = Article.new(params.require[:article].permit(:title,:text))
+	  @article = Article.new(article_params)
+
+	  @article.save
+	  redirect_to @article
+	end
 
 
-def create
-  render plain: params[:article].inspect
-end
-
+	#Refactor for reuse and set private to avoid unintended use.
+	private 
+	def article_params
+		params.require(:article).permit(:title, :text)
+	end
 end
